5037cf15c722ee7aaaa5da02537e67196b2210db
Ela-El-maker <feloela101@gmail.com>
2024-01-25 23:44:28 +0300

    Fix006: Reset Password

--- resources/views/admin/profile/profile.blade.php
+++ resources/views/admin/profile/profile.blade.php
@@ -24,7 +24,7 @@
             <div class="card">
               <form method="post"  class="needs-validation" novalidate="">
                 <div class="card-header">
-                  <h4>Edit Profile</h4>
+                  <h4>Profile Information</h4>
                 </div>
                 <div class="card-body">
                   <form action="{{ route('profile.update') }}" method="POST">
@@ -46,6 +46,61 @@
                             <code>{{$errors->first('email')}}</code>                          
                         @endif
                       </div>
+                   
+                    </div>
+                    <div class="card-footer text-right">
+                      <button class="btn btn-primary">Save Changes</button>
+                    </div>
+                  </form>
+                    
+                    
+                </div>
+                
+              </form>
+            </div>
+            <div class="card">
+              <form method="post"  class="needs-validation" novalidate="">
+                <div class="card-header">
+                  <h4>Profile Information</h4>
+                </div>
+                <div class="card-body">
+                  <form action="{{ route('profile.update') }}" method="POST">
+                    @csrf
+                    @method('patch')
+                    <div class="row">
+                      <div class="form-group col-md-6 col-12">
+                        <label>Current Password</label>
+                        <input type="password" class="form-control" name="current_password" value="{{old('name',$user->name)}}" required="">
+                        @if ($errors->updatePassword->has('current_password'))
+                            <code>{{$errors->updatePassword->first('current_password')}}</code>                          
+                        @endif
+                      </div>
+                      <div class="form-group col-md-6 col-12">
+                        <label>Name</label>
+                        <input type="password" class="form-control" name="current_password" value="{{old('name',$user->name)}}" required="">
+                        @if ($errors->updatePassword->has('current_password'))
+                            <code>{{$errors->updatePassword->first('current_password')}}</code>                          
+                        @endif
+                      </div>
+                      <div class="form-group col-md-6 col-12">
+                        <label>Name</label>
+                        <input type="password" class="form-control" name="current_password" value="{{old('name',$user->name)}}" required="">
+                        @if ($errors->updatePassword->has('current_password'))
+                            <code>{{$errors->updatePassword->first('current_password')}}</code>                          
+                        @endif
+                      </div>
+
+                      <div>
+                        <x-input-label for="update_password_password" :value="__('New Password')" />
+                        <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                        <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+                    </div>
+            
+                    <div>
+                        <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
+                        <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                        <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+                    </div>
                    
                     </div>
                     <div class="card-footer text-right">
