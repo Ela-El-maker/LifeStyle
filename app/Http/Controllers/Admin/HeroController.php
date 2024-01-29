cb203e87088ccd7581c19bbcc983925f75b1d617
Ela-El-maker <feloela101@gmail.com>
2024-01-29 23:33:00 +0300

    Fix012: Hero Section Part 3

--- app/Http/Controllers/Admin/HeroController.php
+++ app/Http/Controllers/Admin/HeroController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hero;
+use Faker\Core\File as CoreFile;
+use Faker\Provider\File as ProviderFile;
 use Illuminate\Http\Request;
+use File;
+use Illuminate\Http\File as HttpFile;
+use Illuminate\Http\Testing\File as TestingFile;
 
 class HeroController extends Controller
 {
@@ -13,7 +19,8 @@ class HeroController extends Controller
     public function index()
     {
         //
-        return view('admin.hero.index');
+        $hero = Hero::first();
+        return view('admin.hero.index', compact('hero'));
     }
 
     /**
@@ -53,7 +60,45 @@ class HeroController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        
+        $request -> validate([
+            'title' => ['required','string','max:255'],
+            'sub_title' => ['required','string','max:500'],
+            'image' => ['max:3000','image'],
+        ]);
+        //dd($request ->all());
+        if($request -> hasFile('image')){
+            // dd("Hello World");
+            $hero = Hero::first();
+               if($hero  && File::exists(public_path($hero -> image))){
+                   File::delete(public_path($hero -> image));
+               }
+   
+               $image = $request -> file('image');
+               $imageName = rand().$image -> getClientOriginalName();
+               $image -> move(public_path('/uploads'), $imageName);
+   
+               $imagePath = "/uploads/" . $imageName;
+            //    dd($imagePath);
+               Hero::updateorCreate(
+                  ['id' => $id],
+                  [
+                       'title' => $request -> title,
+                       'sub_title' => $request -> sub_title,
+                       'btn_text' => $request -> btn_text,
+                       'btn_url' => $request -> btn_url,
+                       'image' => isset($imagePath) ? $imagePath : '',
+                   ]
+                   );
+
+                   //dd('success');
+                //Display a success toast, with a title
+                toastr()->success('Updated Successfully', 'Congrats');
+                return redirect() -> back();
+          
+         
+        
+        }
     }
 
     /**
