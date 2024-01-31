1a666b975a7e6a58c09a927c2adc55ecd5588ddc
Ela-El-maker <feloela101@gmail.com>
2024-02-01 00:55:25 +0300

    Fix016: Done with Hero Section

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,10 @@ import './bootstrap';
 
 
 
-import 'laravel-datatables-vite';
+// import 'laravel-datatables-vite';
+import 'https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js'
+import 'https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js'
+// import 'https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js'
 
 import Alpine from 'alpinejs';
 
