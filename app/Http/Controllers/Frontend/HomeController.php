4033b1ed755886cc1d6efcdcf126a3d4b5ed080e
Ela-El-maker <feloela101@gmail.com>
2024-02-04 02:55:18 +0300

    Fix022: Feedback Worked Section

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\About;
 use App\Models\Category;
 use App\Models\Experience;
+use App\Models\Feedback;
+use App\Models\FeedbackSectionSetting;
 use App\Models\Hero;
 use App\Models\PortfolioItem;
 use App\Models\PortfolioSectionSetting;
@@ -30,6 +32,8 @@ class HomeController extends Controller
         $skill = SkillSectionSetting::first();
         $skillItems = SkillItem::all();
         $experience = Experience::first();
+        $feedbacks = Feedback::all();
+        $feedbackTitle = FeedbackSectionSetting::first();
         return view(
             'frontend.home', 
                 compact(
@@ -43,6 +47,8 @@ class HomeController extends Controller
                     'skill' ,
                     'skillItems',
                     'experience',
+                    'feedbacks',
+                    'feedbackTitle',
                 ));
     }
     public function showPortfolio($id){
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Admin\AboutController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ExperienceController;
+use App\Http\Controllers\Admin\FeedbackController;
+use App\Http\Controllers\Admin\FeedbackSectionSettingController;
 use App\Http\Controllers\Admin\HeroController;
 use App\Http\Controllers\Admin\PortfolioItemController;
 use App\Http\Controllers\Admin\PortfolioSectionSettingController;
@@ -89,4 +91,10 @@ Route::group(['middleware' => ['auth'],'prefix' => 'admin', 'as' => 'admin.'], f
     Route::resource('experience', ExperienceController::class);
     
 
+    /**Feedback Route */
+    Route::resource('feedback', FeedbackController::class);
+
+    /**Feedback Section Settings Route */
+    Route::resource('feedback-section-setting', FeedbackSectionSettingController::class);
+    
 });
